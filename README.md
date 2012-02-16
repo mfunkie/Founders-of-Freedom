681bab57a0773b7c6c5a552f5797179a48fb6fca
Mark Funk <mfunk86@gmail.com>
2012-02-16 06:20:13 -0500

    Updating readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Working on making Founders of Freedom, a Catan clone built in node.js
+
+Until I set proper dependencies up, I have set the following to required:
+    Express
+    roll
--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express')
     app = express.createServer();
 
 
-app.get('/', function(req res) {
+app.get('/', function(req, res) {
+    console.log('A wild user has appeared!');
     res.send('Hello World');
 });
 
