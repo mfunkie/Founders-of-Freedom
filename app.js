c44dde9f09302d22089df383938389ad7c8dcf82
Mark Funk <mfunk86@gmail.com>
2012-02-16 07:01:40 -0500

    gitignore changes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express')
+var express = require('express'),
+    colors = require('colors'),
     app = express.createServer();
-
 
 app.get('/', function(req, res) {
     console.log('A wild user has appeared!');
-    res.send('Hello World');
+    res.send("Hello World!");
+    console.log("DERP".blue);
 });
 
 app.listen(3000);
